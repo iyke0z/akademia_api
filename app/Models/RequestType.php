ba844fa5f1c31f48a755e7200b53e506feb76e63
Idowu John Oluwatosin <idowujohn9@gmail.com>
2021-11-18 23:36:07 +0100

    Models Created 35/53

--- app/Models/RequestType.php
+++ app/Models/RequestType.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Passport\HasApiTokens;
 
 class RequestType extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    use HasApiTokens;
+    
+    protected $fillable = [
+        'branchid',
+        'reqtype',
+        'createdById',
+    ];
+
+
+    protected $hidden = [
+    ];
+
+    // Model relationships
+    public function branch()
+    {
+    return $this->belongsTo(Branch::class, 'branchid', 'id');
+    }
+    public function created()
+    {
+    return $this->belongsTo(User::class, 'createdById', 'id');
+    }
 }
--- app/Models/Result.php
+++ app/Models/Result.php
@@ -4,8 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Passport\HasApiTokens;
 
 class Result extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    use HasApiTokens;
+    
+    protected $fillable = [
+        'restult_typeid',
+        'studentid',
+        'academic_periodid',
+        'subjectid',
+        'score_obtained',
+        'createdById',
+    ];
+
+
+    protected $hidden = [
+    ];
+
+    // Model relationships
+    public function resultType()
+    {
+    return $this->belongsTo(RequestType::class, 'branchid', 'id');
+    }
+    public function student()
+    {
+    return $this->belongsTo(User::class, 'studentid', 'id');
+    }
+    public function academicYear()
+    {
+    return $this->belongsTo(Term_Session_Year::class, 'academic_periodid', 'id');
+    }
+    public function subject()
+    {
+    return $this->belongsTo(Subject::class, 'subjectid', 'id');
+    }
+    public function created()
+    {
+    return $this->belongsTo(User::class, 'createdById', 'id');
+    }
 }
